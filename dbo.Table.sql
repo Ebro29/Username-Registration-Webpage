aa3f24af8197a1e1ef6704a2d09b2964b83b74df
Ebro29 <56973777+Ebro29@users.noreply.github.com>
2021-04-25 08:09:26 -0500

    Add files via upload

---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Admin_ID] NVARCHAR(50) NULL, 
+    [Admin_Username] NVARCHAR(50) NULL, 
+    [Admin_Password] NVARCHAR(50) NULL, 
+    [Admin_FName] NVARCHAR(50) NULL, 
+    [Admin_LName] NVARCHAR(50) NULL, 
+    [Admin_Email] NVARCHAR(50) NULL, 
+    [Admin_Country] NVARCHAR(50) NULL
+)
